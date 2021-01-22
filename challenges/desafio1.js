a7d547ab3f7657fda792732e9277a7cd4f1ad515
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-22 15:39:17 -0300

    feat: update criadoPor

---
+++ challenges/desafio1.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      criadoPor: "Ronald McDonald",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: true,
+    criadoPor: true,
+  },
+);
