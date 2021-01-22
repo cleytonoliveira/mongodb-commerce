5cb8e57ccb9cff416fca91fc74c8c38f18a5c432
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-01-22 17:47:51 -0300

    feat: update ketchup

---
+++ challenges/desafio5.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  { nome:
+    { $ne: "McChicken" },
+  },
+  { $addToSet:
+    { ingredientes: "ketchup" },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: true,
+    ingredientes: true,
+  },
+);
